f5cecf45f343d2c5de49aa64ef381669d71ba426
jdbuenol <JulianDavidBueno@gmail.com>
2023-06-01 08:15:26 -0500

    jobs Dane

---
+++ src/danePuntoA.py
@@ -0,0 +1,4 @@
+from jobs.daneJobs import MRAverageSE
+
+if __name__ == '__main__':
+    MRAverageSE.run()
---
+++ src/danePuntoC.py
@@ -0,0 +1,4 @@
+from jobs.daneJobs import MRSEPerEmployee
+
+if __name__ == '__main__':
+    MRSEPerEmployee.run()
